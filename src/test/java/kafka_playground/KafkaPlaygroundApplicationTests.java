3bab3b1b931f15f3dee29117645ca4700bbd92c4
깃짱 <eunkeeeee@gmail.com>
2025-11-05 16:07:31 +0900

    feat: init

---
+++ src/test/java/kafka_playground/KafkaPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package kafka_playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
